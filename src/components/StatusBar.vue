962fbf83a3c221e1b414a6cdf79a453b9dd8c14b
avv <evgenyi.a@bitsgap.com>
2026-05-20 14:22:13 +0300

    chore: поднять версию до 0.2.44

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -46,7 +46,7 @@ const behind = computed(() => currentBranchInfo.value?.behind ?? 0);
     </div>
 
     <div class="statusbar-right">
-      <span class="version">0.1.43</span>
+      <span class="version">0.2.44</span>
     </div>
   </div>
 </template>
